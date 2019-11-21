6a37282714f80d344ad31a8b50af469b9783e408
Hiren Pattani <pattanihiren1@gmail.com>
2019-11-21 08:19:22 +0530

    [ADD]cpi_otherinfoso: Added cpi_otherinfoso module.

---
+++ cpi_otherinfoso/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cpi_otherinfoso",
+
+    'summary': """
+        Modifier Sale Order""",
+
+    'description': """
+        Modifier Sale Order
+    """,
+
+    'author': "Hiren/Hashmicro",
+    'website': "http://www.hashmicro.com",
+
+    'category': 'Sales',
+    'version': '10.0.1.0.0',
+
+    'depends': ['sale_stock'],
+
+    'data': [
+        'views/views.xml',
+    ],
+}
